e5e4cbe8a01ac0e98015d2597b1b44ec8b491530
qianfeifan <893645913@qq.com>
2017-08-21 14:09:22 +0800

    更新 payment.php

--- trunk/protected/controllers/payment.php
+++ trunk/protected/controllers/payment.php
@@ -494,9 +494,9 @@ class PaymentController extends Controller {
         //支付成功
         if ($return == 1) {
             $model = new Model("order");
-            $order = $model->where("order_no='{$orderNo}'")->find();
-            if($order['type']==7){
-             $result=Common::setIncomeByInviteShip1($order);
+            $orders = $model->where("order_no='{$orderNo}'")->find();
+            if($orders['type']==7){
+             $result=Common::setIncomeByInviteShip1($orders);
             }
             if (stripos($orderNo, 'promoter') !== false) {//如果是推广员入驻订单
                 $order = $this->model->table("district_order")->where("order_no ='" . $orderNo . "'")->find();
