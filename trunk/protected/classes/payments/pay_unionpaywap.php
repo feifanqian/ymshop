ff5dd90b7abea0d90e6afc9a7b8466bd8b5a87be
qianfeifan <893645913@qq.com>
2017-09-12 16:14:51 +0800

    qff

--- trunk/protected/classes/payments/pay_unionpaywap.php
+++ trunk/protected/classes/payments/pay_unionpaywap.php
@@ -14,8 +14,8 @@ class pay_unionpaywap extends PaymentPlugin {
 
     //提交地址
     public function submitUrl() {
-        return 'https://101.231.204.80:5000/gateway/api/frontTransReq.do'; //测试环境请求地址
-        // return 'https://gateway.95516.com/gateway/api/frontTransReq.do'; //生产环境
+        // return 'https://101.231.204.80:5000/gateway/api/frontTransReq.do'; //测试环境请求地址
+        return 'https://gateway.95516.com/gateway/api/frontTransReq.do'; //生产环境
     }
 
     //取得配制参数
