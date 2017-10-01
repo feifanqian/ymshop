fb56980326e61f0c8cad60e3e6e0d1bd2dd6e103
qianfeifan <893645913@qq.com>
2017-10-01 11:08:08 +0800

    qff

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -97,22 +97,22 @@ class UcenterController extends Controller {
     public function buildinvite() {
         $user_id = Filter::int(Req::args('uid'));
         // var_dump($user_id);die;
-        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
-            // var_dump(123);die;
-            $wechatcfg = $this->model->table("oauth")->where("class_name='WechatOAuth'")->find();
-            $wechat = new WechatMenu($wechatcfg['app_key'], $wechatcfg['app_secret'], '');
-            $token = $wechat->getAccessToken();
-            $params = array(
-                "action_name" => "QR_LIMIT_STR_SCENE",
-                "action_info" => array("scene" => array("scene_str" => "invite-{$user_id}"))
-            );
-            $ret = Http::curlPost("https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={$token}", json_encode($params));
-            $ret = json_decode($ret, TRUE);
-            if (isset($ret['ticket'])) {
-                $this->redirect("https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket={$ret['ticket']}");
-                exit;
-            }
-        }
+        // if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
+        //     // var_dump(123);die;
+        //     $wechatcfg = $this->model->table("oauth")->where("class_name='WechatOAuth'")->find();
+        //     $wechat = new WechatMenu($wechatcfg['app_key'], $wechatcfg['app_secret'], '');
+        //     $token = $wechat->getAccessToken();
+        //     $params = array(
+        //         "action_name" => "QR_LIMIT_STR_SCENE",
+        //         "action_info" => array("scene" => array("scene_str" => "invite-{$user_id}"))
+        //     );
+        //     $ret = Http::curlPost("https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={$token}", json_encode($params));
+        //     $ret = json_decode($ret, TRUE);
+        //     if (isset($ret['ticket'])) {
+        //         $this->redirect("https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket={$ret['ticket']}");
+        //         exit;
+        //     }
+        // }
         $url = Url::fullUrlFormat("/index/invite") . "?inviter_id=" . $user_id;
         $qrCode = new QrCode();
         $qrCode
