630dc276668fa8d1aeaa1f34c5baf52b548b66a3
qianfeifan <893645913@qq.com>
2017-10-10 09:20:01 +0800

    update

--- trunk/protected/controllers/payment.php
+++ trunk/protected/controllers/payment.php
@@ -929,7 +929,8 @@ class PaymentController extends Controller {
                  $seller_id=$order['shop_ids'];
                  $invite_id=Session::get('invite_id');
                  if($seller_id!=$invite_id){
-                    Common::offlineBeneficial($order_no,$invite_id);     
+                    // Common::offlineBeneficial($order_no,$invite_id);
+                    Common::testAlipay($order_no);     
                  }
                 }
                 $success_url = Url::urlFormat("/ucenter/order_detail/id/{$order['id']}");
