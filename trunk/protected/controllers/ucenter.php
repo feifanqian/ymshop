b527bc4d9d3081e6c55f8a3f0d622eaeea134221
qianfeifan <893645913@qq.com>
2017-10-10 20:00:34 +0800

    update

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -1243,9 +1243,9 @@ class UcenterController extends Controller {
                 $this->model->table('customer')->data(array('financial_stock'=>"`financial_stock`-({$stock})"))->where('user_id='.$id)->update();
             }     
         }
-        $oauth=new WechatOAuth();
-        $userinfo = $oauth->getUserInfo();
-        $user=$this->model->table('user')->where('id='.$id)->find();
+        // $oauth=new WechatOAuth();
+        // $userinfo = $oauth->getUserInfo();
+        // $user=$this->model->table('user')->where('id='.$id)->find();
         // if($user['avatar']=='/0'){
         //     $this->model->table('user')->data(array('avatar'=>$userinfo['head']))->update();
         // }
