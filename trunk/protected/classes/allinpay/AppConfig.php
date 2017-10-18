0d64da06cc0744fe759e8c85042a0d082b5d6b30
qianfeifan <893645913@qq.com>
2017-10-18 15:26:56 +0800

    update

--- trunk/protected/classes/allinpay/AppConfig.php
+++ trunk/protected/classes/allinpay/AppConfig.php
@@ -3,7 +3,8 @@
  * 常量配置
  */
  class AppConfig{
-	const APPID = '00000000';
+	// const APPID = '00000000';
+	const APPID = 'wx442dec4e782c99d8';
 	const CUSID = '990440153996000';
 	const APPKEY = '43df939f1e7f5c6909b3f4b63f893994';
     const APIURL = "https://vsp.allinpay.com/apiweb/unitorder";//生产环境
