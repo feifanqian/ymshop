ea502a04b3981c3cf216a1302ffd76a3d4af15c5
qianfeifan <893645913@qq.com>
2017-10-19 17:30:30 +0800

    update

--- trunk/protected/classes/payments/pay_wxpayjsapi.php
+++ trunk/protected/classes/payments/pay_wxpayjsapi.php
@@ -76,7 +76,7 @@ class pay_wxpayjsapi extends PaymentPlugin {
         //基本参数
         $return['return_url'] = $this->callbackUrl . "/out_trade_no/" . $payment['M_OrderNO'];
         $return['notify_url'] = $this->asyncCallbackUrl;
-        var_dump($return['notify_url']);die;  
+          
         $return['subject'] = $payment['R_Name'];
         $return['out_trade_no'] = substr(time(), 6) . "_" . $payment['M_OrderNO'];
         $return['price'] = number_format($payment['M_Amount'], 2, '.', '');
