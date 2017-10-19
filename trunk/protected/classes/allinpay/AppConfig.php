7912040f81c5031510f24e57398dd5bd4827f6f1
qianfeifan <893645913@qq.com>
2017-10-19 16:37:13 +0800

    update

--- trunk/protected/classes/allinpay/AppConfig.php
+++ trunk/protected/classes/allinpay/AppConfig.php
@@ -3,10 +3,14 @@
  * 常量配置
  */
  class AppConfig{
-	const APPID = '479584053115558';
+	const APPID = '00000000';
 	// const APPID = 'wx442dec4e782c99d8';
-	const CUSID = '200604000000445';
-	const APPKEY = '43df939f1e7f5c6909b3f4b63f893994';
+	// const CUSID = '1485608362';
+	const CUSID = '200584000015505';
+	// const CUSID = '200604000000445';
+	// const CUSID = '479584053115558';
+	// const APPKEY = '43df939f1e7f5c6909b3f4b63f893994';
+	const APPKEY = '835b565520c99db487f58d5ca6f8a67c';
     const APIURL = "https://vsp.allinpay.com/apiweb/unitorder";//生产环境
     const APIVERSION = '11';
  }
