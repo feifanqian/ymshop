92b9914d2e6f2d4c5c9ec5e829e44d344f0864a9
qianfeifan <893645913@qq.com>
2017-10-19 19:17:08 +0800

    update

--- trunk/protected/classes/allinpay/AppConfig.php
+++ trunk/protected/classes/allinpay/AppConfig.php
@@ -10,8 +10,8 @@
 	const CUSID = '990440153996000';
 	// const CUSID = '200604000000445';
 	// const CUSID = '479584053115558';
-	// const APPKEY = '43df939f1e7f5c6909b3f4b63f893994';
-	const APPKEY = '835b565520c99db487f58d5ca6f8a67c';
+	const APPKEY = '43df939f1e7f5c6909b3f4b63f893994';
+	// const APPKEY = '835b565520c99db487f58d5ca6f8a67c';
     const APIURL = "https://vsp.allinpay.com/apiweb/unitorder";//生产环境
     const APIVERSION = '11';
  }
