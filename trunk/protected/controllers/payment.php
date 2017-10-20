385ba360fed489dbc788400099f6753cf5353f2f
qianfeifan <893645913@qq.com>
2017-10-20 11:18:02 +0800

    update

--- trunk/protected/controllers/payment.php
+++ trunk/protected/controllers/payment.php
@@ -881,14 +881,15 @@ class PaymentController extends Controller {
                 $seller_id=$order_model['shop_ids'];
                 $invite_id=Session::get('invite_id');
                 // $invite_id=$order_model['invite_id'];
+                Common::offlineBeneficial($orderNo,$invite_id);
                 //上级代理商是卖家的话不参与分账
                 // if($invite_id==null){
                 //     $invite_id=1;
                 // }
-                $promoter_id=Common::getFirstPromoter($order_model['user_id']);
-                if($seller_id!=$promoter_id){
-                    Common::offlineBeneficial($orderNo,$invite_id);
-                }
+                // $promoter_id=Common::getFirstPromoter($order_model['user_id']);
+                // if($seller_id!=$promoter_id){
+                //     Common::offlineBeneficial($orderNo,$invite_id);
+                // }
             }
             if (stripos($orderNo, 'promoter') !== false) {
                 $order = $this->model->table("district_order")->where("order_no ='" . $orderNo . "'")->find();
