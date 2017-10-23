7d323ef0eb659bfe25414031719641ca930aa95b
qianfeifan <893645913@qq.com>
2017-10-23 09:42:19 +0800

    qff

--- trunk/protected/controllers/payment.php
+++ trunk/protected/controllers/payment.php
@@ -1150,7 +1150,7 @@ class PaymentController extends Controller {
                     // var_dump($payinfo);die;
                     $jsApiParameters=json_encode($payinfo);
                     // $jsApiParameters=$payinfo;
-                    var_dump($jsApiParameters);die;
+                    // var_dump($jsApiParameters);die;
                 }  
             }
             // var_dump($jsApiParameters);die;
