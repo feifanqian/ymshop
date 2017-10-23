38d5e21d36cfe8552de61996c6901d81e64fc3f7
qianfeifan <893645913@qq.com>
2017-10-23 11:45:32 +0800

    qff

--- trunk/protected/classes/Common.php
+++ trunk/protected/classes/Common.php
@@ -1183,4 +1183,12 @@ class Common {
         
         return FALSE;
     }
+
+    public function xmlToArray($xml) {
+
+    //禁止引用外部xml实体
+    libxml_disable_entity_loader(true);
+    $values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+    return $values;
+    }
 }
