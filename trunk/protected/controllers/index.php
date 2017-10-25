beca5154bc9d73347756d4e62d9b2cef29d5b9ca
qianfeifan <893645913@qq.com>
2017-10-25 20:02:01 +0800

    qff

--- trunk/protected/controllers/index.php
+++ trunk/protected/controllers/index.php
@@ -1824,7 +1824,7 @@ class IndexController extends Controller {
         $model = new Model();
        Session::set('demo', 1);
        $inviter_id = intval(Req::args('inviter_id'));
-       // var_dump($inviter_id);die;
+       var_dump($inviter_id);die;
         if (isset($this->user['id'])) {
             Common::buildInviteShip($inviter_id, $this->user['id'], "second-wap");
             // var_dump($inviter_id);die;      
