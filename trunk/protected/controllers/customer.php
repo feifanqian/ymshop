95c55a9ca6c261ac9cfd4a1c9f9821300a32c3f0
qianfeifan <893645913@qq.com>
2017-10-27 13:46:32 +0800

    qff

--- trunk/protected/controllers/customer.php
+++ trunk/protected/controllers/customer.php
@@ -592,7 +592,7 @@ class CustomerController extends Controller {
                 'DATA_TYPE' => '2',
                 'LEVEL' => '6',
                 // 'USER_NAME' => '20060400000044502',
-                'USER_NAME' => '47958405311555802',
+                'USER_NAME' => '20058400001550502',
                 // 'USER_PASS' => '`12qwe',
                 'USER_PASS' => '111111',
                 'REQ_SN' => $merchantId.date('YmdHis').rand(1000,9999),
