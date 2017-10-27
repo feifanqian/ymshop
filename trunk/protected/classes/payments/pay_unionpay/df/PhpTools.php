280f93a99e56e85e189ab73a96e76b0c3721209b
qianfeifan <893645913@qq.com>
2017-10-27 16:18:40 +0800

    qff

--- trunk/protected/classes/payments/pay_unionpay/df/PhpTools.php
+++ trunk/protected/classes/payments/pay_unionpay/df/PhpTools.php
@@ -125,6 +125,7 @@ class PhpTools{
 	public function send($params){
 		$xmlSignPost=$this->signXml($params);
 		$xmlSignPost=str_replace("TRANS_DETAIL2", "TRANS_DETAIL",$xmlSignPost);
+		var_dump($xmlSignPost);die;
 		$response = cURL::factory()->post(PhpTools::apiUrl, $xmlSignPost);
 	
 		if (! isset($response['body'])) {
