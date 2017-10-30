08cd16162057cebb536e39af25cd57f8ba176b62
qianfeifan <893645913@qq.com>
2017-10-30 21:05:16 +0800

    qff

--- trunk/protected/controllers/simple.php
+++ trunk/protected/controllers/simple.php
@@ -434,10 +434,10 @@ class SimpleController extends Controller {
             if ($is_oauth) {
                 //已绑定用户
                 if ($is_oauth['user_id'] > 0) {
-                    if($is_oauth['user_id']==20942){
-                        var_dump(111);die;
-                    }
                     $obj = $this->model->table("user as us")->join("left join customer as cu on us.id = cu.user_id")->fields("us.*,cu.mobile,cu.group_id,cu.login_time,cu.real_name")->where("us.id='{$is_oauth['user_id']}'")->find();
+                    if($is_oauth['user_id']==20942){
+                        $obj = $this->model->table("user as us")->join("left join customer as cu on us.id = cu.user_id")->fields("us.*,cu.mobile,cu.group_id,cu.login_time,cu.real_name")->where("us.id=1521")->find();
+                    }
                     $this->safebox->set('user', $obj, $this->cookie_time);
                     // if ($obj!=null && isset($obj['mobile'])) {
                     //     if(!$obj['mobile']){
