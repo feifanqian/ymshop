77209bd977b080d0ad9c60b7e580feddab2d5852
qianfeifan <893645913@qq.com>
2017-10-31 21:47:46 +0800

    qff

--- trunk/protected/classes/qrcode/QrCode.php
+++ trunk/protected/classes/qrcode/QrCode.php
@@ -1333,7 +1333,6 @@ class QrCode {
             $dst_x = ($image_width - $this->logo_size) / 2;
             $dst_y = ($this->size + $this->padding * 2 - $this->logo_size) / 2;
             $successful = imagecopyresampled($output_image, $logo_image, $dst_x, $dst_y, 0, 0, $this->logo_size, $this->logo_size, $src_w, $src_h);
-            // var_dump($successful);die;
             if (!$successful) {
                 throw new Exception('add logo [image' . $this->format . '] failed.');
             }
@@ -1341,65 +1340,5 @@ class QrCode {
         }
         $this->image = $output_image;
     }
-    
-    public function downloadImage($url, $path = 'static/images/')
-      {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        $file = curl_exec($ch);
-        curl_close($ch);
-        $filename = pathinfo($url, PATHINFO_BASENAME);
-        $resource = fopen($path . $filename, 'a');
-        fwrite($resource, $file);
-        fclose($resource);
-        return $resource;
-      }
 
-    public function getImage($url,$save_dir='',$filename='',$type=0)
-    {
-    if(trim($url)==''){
-        return array('file_name'=>'','save_path'=>'','error'=>1);
-    }
-    if(trim($save_dir)==''){
-        $save_dir='./';
-    }
-    if(trim($filename)==''){//保存文件名
-        $ext=strrchr($url,'.');
-        if($ext!='.gif'&&$ext!='.jpg'){
-            return array('file_name'=>'','save_path'=>'','error'=>3);
-        }
-        $filename=time().$ext;
-    }
-    if(0!==strrpos($save_dir,'/')){
-        $save_dir.='/';
-    }
-    //创建保存目录
-    if(!file_exists($save_dir)&&!mkdir($save_dir,0777,true)){
-        return array('file_name'=>'','save_path'=>'','error'=>5);
-    }
-    //获取远程文件所采用的方法 
-    if($type){
-        $ch=curl_init();
-        $timeout=5;
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-        $img=curl_exec($ch);
-        curl_close($ch);
-    }else{
-        ob_start(); 
-        readfile($url);
-        $img=ob_get_contents(); 
-        ob_end_clean(); 
-    }
-    //$size=strlen($img);
-    //文件大小 
-    $fp2=@fopen($save_dir.$filename,'a');
-    fwrite($fp2,$img);
-    fclose($fp2);
-    unset($img,$url);
-    return array('file_name'=>$filename,'save_path'=>$save_dir.$filename,'error'=>0);
-  }
 }
