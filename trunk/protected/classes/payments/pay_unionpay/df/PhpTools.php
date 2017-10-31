4ade7769273682bcde7b07277aa253d0a261f3c7
qianfeifan <893645913@qq.com>
2017-10-31 11:25:57 +0800

    qff

--- trunk/protected/classes/payments/pay_unionpay/df/PhpTools.php
+++ trunk/protected/classes/payments/pay_unionpay/df/PhpTools.php
@@ -70,7 +70,7 @@ class PhpTools{
 		$xmlResponse = mb_convert_encoding(str_replace('<?xml version="1.0" encoding="GBK"?>', '<?xml version="1.0" encoding="UTF-8"?>', $xmlResponseSrc), 'UTF-8', 'GBK');
 
 		$results = $this->arrayXml->parseString( $xmlResponse , TRUE);
-
+        var_dump($results);die;
 		if ($flag) {		    
 		    if($results['RET_CODE']==0000){
 		    	$return['status']=1;
