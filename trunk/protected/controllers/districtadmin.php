6f6a50edb90cfdf0f5b4b0c01df8048a346825f9
qianfeifan <893645913@qq.com>
2017-11-02 14:11:56 +0800

    qff

--- trunk/protected/controllers/districtadmin.php
+++ trunk/protected/controllers/districtadmin.php
@@ -262,7 +262,8 @@ class DistrictadminController extends Controller {
         if ($condition_str) {
             $this->assign("where", $condition_str);
         } else {
-            $this->assign("where", "da.unique_code=0");
+            // $this->assign("where", "da.unique_code=0");
+            $this->assign("where", "UNIX_TIMESTAMP(da.create_time)>".1509603047);
         }
         $this->assign("condition", $condition);
         $this->redirect();
