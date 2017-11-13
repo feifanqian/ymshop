f9cc3b72fdc5c75ceb7a63b7a9c2f177de025482
qianfeifan <893645913@qq.com>
2017-11-13 11:24:34 +0800

    qff

--- trunk/protected/classes/payments/pay_unionpay/df/AllinpayDf.php
+++ trunk/protected/classes/payments/pay_unionpay/df/AllinpayDf.php
@@ -13,7 +13,7 @@ class AllinpayDf{
                 'LEVEL' => '6',
                 'USER_NAME' => '20058400001550504',
                 'USER_PASS' => '111111',
-                'REQ_SN' => $params['withdraw_no'].date('YmdHis').rand(1000,9999),
+                'REQ_SN' => $merchantId.substr($params['withdraw_no'],2),
             ),
             'TRANS' => array(
                 'BUSINESS_CODE' => '09900',
