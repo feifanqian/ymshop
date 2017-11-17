53edbe58e9bdf7ac2bbfe1c90080325607601a60
qianfeifan <893645913@qq.com>
2017-11-17 17:59:15 +0800

    qff

--- trunk/protected/controllers/customer.php
+++ trunk/protected/controllers/customer.php
@@ -206,8 +206,8 @@ class CustomerController extends Controller {
                         }
                     }else{
                         $model->query("update tiny_balance_withdraw set status='2' where id = $id and status= 0");
-                        $model->table('customer')->data(array('offline_balance' => "`offline_balance`+" . $obj['amount']))->where('user_id=' . $obj['user_id'])->update();
-                        Log::balance($obj['amount'], $obj['user_id'],$obj['withdraw_no'],"余额提现失败退回", 3, $this->manager['id']);
+                        // $model->table('customer')->data(array('offline_balance' => "`offline_balance`+" . $obj['amount']))->where('user_id=' . $obj['user_id'])->update();
+                        // Log::balance($obj['amount'], $obj['user_id'],$obj['withdraw_no'],"余额提现失败退回", 3, $this->manager['id']);
                         exit(json_encode(array('status'=>'fail','msg'=>$result['msg'])));
                     }
                 }else if($status=="-1"){
