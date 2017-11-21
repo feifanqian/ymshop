5308aa2311c1e4cc10482b7df9c28e633feaf172
yjn0428 <yjn0428@aliyun.com>
2017-11-21 19:56:30 +0800

    ok

--- trunk/protected/actions/paytonglian.php
+++ trunk/protected/actions/paytonglian.php
@@ -466,13 +466,13 @@ class PaytonglianAction extends Controller
         $client->setSysId($this->sysid);
         $client->setSignMethod($this->signMethod);
         $param["bizUserId"] = $bizUserId;
-
+        print_r($obj);die();
         $param["tranceNum"] = $tranceNum;
         $param["transDate"] = $transDate;
         $param["phone"] = $phone;
         $param["verificationCode"] = $verificationCode;
         $result = $client->request("MemberService", "bindBankCard", $param);
-        print_r($obj);die();
+
         if ($result['status'] == 'OK') {
             $this->code = 0;
         } else {
