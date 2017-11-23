d9cc46f2f3d9bcc52a40234502ae2034a789baff
qianfeifan <893645913@qq.com>
2017-11-23 21:35:17 +0800

    qff

--- trunk/protected/classes/class_index.php
+++ trunk/protected/classes/class_index.php
@@ -128,7 +128,7 @@ return array(
     "ChinapayDf" => "payments/pay_unionpay/df/ChinapayDf.php",
     "netpayclient" => "payments/pay_unionpay/df/netpayclient.php",
     "AopClient"=>"Alipay/aop/AopClient.php",
-    "AlipayTradeAppPayRequest"=>"payments/Alipay/aop/AlipayTradeAppPayRequest.php",
+    // "AlipayTradeAppPayRequest"=>"payments/Alipay/aop/AlipayTradeAppPayRequest.php",
     "AlipayFundTransToaccountTransferRequest"=>"Alipay/aop/request/AlipayFundTransToaccountTransferRequest.php",
     "AopSdk"=>"Alipay/AopSdk.php",
     "AppConfig"=>"allinpay/AppConfig.php",
