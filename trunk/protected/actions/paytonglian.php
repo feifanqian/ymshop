8a17c32e436f134eaa1f7812e2fc072f1d8a1544
yjn0428 <yjn0428@aliyun.com>
2017-11-24 16:40:12 +0800

    update

--- trunk/protected/actions/paytonglian.php
+++ trunk/protected/actions/paytonglian.php
@@ -1215,10 +1215,10 @@ class PaytonglianAction extends Controller
 
         $user_id = Req::args('user_id');
         $bizUserId = Req::args('bizUserId');
+        $bizOrderNo = Req::args('bizOrderNo');
         $model = new Model();
         $obj = $this->model->table('tradeno')->fields('trade_no,biz_orderno')->where("user_id='$user_id'AND biz_orderno='$bizOrderNo'")->find();
         $tradeNo = $obj['trade_no'];
-        $bizOrderNo = $obj['biz_orderno'];
         $verificationCode = Req::args('verificationCode');
         $consumerIp = Req::args('consumerIp');
         $param = array(
