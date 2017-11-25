05acc3fbf333605cf46a22dc48e35f36659bd9cb
qianfeifan <893645913@qq.com>
2017-11-25 14:39:40 +0800

    qff

--- trunk/protected/actions/product.php
+++ trunk/protected/actions/product.php
@@ -483,6 +483,26 @@ class ProductAction extends Controller {
         
     }
 
+    public function wei() {
+        $page = Filter::int(Req::args("page"));
+        $page = $page < 0 ? 1 : $page;
+        $page_size = 10;
+        
+        $list = $this->model->table("pointwei_sale as ps")->fields("ps.*,go.name,go.img,go.sell_price")->join("left join goods as go on ps.goods_id = go.id")->where("go.is_online = 0 and go.is_weishang = 1 and ps.status = 1 and go.store_nums > 0")->order("ps.listorder asc")->findPage($page, $page_size);
+        unset($list['html']);
+        if ($list['data']) {
+            foreach ($list['data'] as $k => &$v) {
+                $v['imgs'] = array_values(unserialize($v['imgs']));
+                unset($v['specs'], $v['attrs'], $v['content']);
+            }
+        }
+        $this->code = 0;
+        $this->content = array(
+            'weilist' => $list,
+            );
+        
+    }
+
     public function group() {
         $page = Filter::int(Req::args("page"));
         $page = $page < 0 ? 1 : $page;
