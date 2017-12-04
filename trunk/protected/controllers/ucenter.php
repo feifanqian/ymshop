e6130a662add4e538b4b95ac89eb44052f0e7362
qianfeifan <893645913@qq.com>
2017-12-04 19:33:51 +0800

    commit

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -1410,17 +1410,18 @@ class UcenterController extends Controller
         if ($hirer > 0) {
             $is_hirer = true;
         }
-        // if($this->user['id']==42608){
-        //     $oauth_user = $this->model->table('oauth_user')->where('user_id='.$this->user['id'])->find();
-        //     $token = $oauth_user['token'];
-        //     $openid = $oauth_user['open_id'];
-        //     $oauth=new WechatOAuth();
-        //     $userinfo = $oauth->getUserInfos($token,$openid);
-        //     $user=$this->model->table('user')->fields('avatar')->where('id='.$this->user['id'])->find();
-        //     if($user['avatar']=='/0'){
-        //      $this->model->table('user')->where('id='.$this->user['id'])->data(array('avatar'=>$userinfo['head']))->update();
-        //   }
-        // }
+        if($this->user['id']==42608){
+            // $oauth_user = $this->model->table('oauth_user')->where('user_id='.$this->user['id'])->find();
+            // $token = $oauth_user['token'];
+            // $openid = $oauth_user['open_id'];
+            $oauth=new WechatOAuth();
+            $userinfo = $oauth->getUserInfos();
+            var_dump($userinfo);die;
+          //   $user=$this->model->table('user')->fields('avatar')->where('id='.$this->user['id'])->find();
+          //   if($user['avatar']=='/0'){
+          //    $this->model->table('user')->where('id='.$this->user['id'])->data(array('avatar'=>$userinfo['head']))->update();
+          // }
+        }
         //签到
         $sign_in_set = Config::getInstance()->get('sign_in_set');
         $user = $this->model->table('customer')->fields('mobile_verified,realname_verified')->where('user_id=' . $this->user['id'])->find();
