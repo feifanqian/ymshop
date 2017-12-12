8cbe77955c4de710ef34ef0b31f40bc278023651
qianfeifan <893645913@qq.com>
2017-12-12 17:58:01 +0800

    commit

--- trunk/protected/actions/address.php
+++ trunk/protected/actions/address.php
@@ -749,4 +749,13 @@ class AddressAction extends Controller
         $this->content = $county;
     }
 
+    public function getLnglat(){
+        $address = Filter::text(Req::args('address'));
+        // $key = "30e9de56560b226c08a389ee23550f68";
+        $url = "http://restapi.amap.com/v3/geocode/geo?address=".$address."&output=JSON&key=30e9de56560b226c08a389ee23550f68";
+        $result = file_get_contents($url);
+        $this->code = 0;
+        $this->content = $result;
+    }
+
 }
