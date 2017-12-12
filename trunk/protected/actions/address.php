709be9872a9f07ac558a1ed7d960b855e8abcad1
qianfeifan <893645913@qq.com>
2017-12-12 18:20:09 +0800

    commit

--- trunk/protected/actions/address.php
+++ trunk/protected/actions/address.php
@@ -755,6 +755,7 @@ class AddressAction extends Controller
         $url = "http://restapi.amap.com/v3/geocode/geo?address=".$address."&output=JSON&key=30e9de56560b226c08a389ee23550f68";
         $result = file_get_contents($url);
         $return = json_decode($result,true);
+        var_dump($return['geocodes']);die;
         $location = $return['geocodes']['location'];
         $str = explode(',',$location);
         $lng = $str[0];
