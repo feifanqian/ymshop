66ae017fd2afe2fc60329d847f144be561b9e50f
qianfeifan <893645913@qq.com>
2017-12-20 14:45:46 +0800

    commit

--- trunk/protected/classes/roles/Promoter.php
+++ trunk/protected/classes/roles/Promoter.php
@@ -277,7 +277,7 @@ class Promoter extends Object {
     public function getMyInviteList($page=1){
         $record = $this->model->table('invite as do')
                 ->join('left join user as u on do.invite_user_id = u.id')
-                ->fields('u.avatar,u.nickname,do.createtime')
+                ->fields('u.id,u.avatar,u.nickname,do.createtime')
                 ->where("do.user_id=".$this->user_id)
                 ->order("do.id desc")
                 ->findPage($page, 10);
@@ -286,6 +286,19 @@ class Promoter extends Object {
         }
         if (isset($record['html'])) {
             unset($record['html']);
+        }
+        if($record['data']){
+            foreach($record['data'] as $k=>$v){
+                $shop = $this->model->table('district_shop')->where('owner_id='.$v['id'])->find();
+                $promoter = $this->model->table('district_promoter')->where('user_id='.$v['id'])->find();
+                if($shop && $promoter){
+                    $record['data'][$k]['role_type'] = 2;
+                }elseif(!$shop && $promoter){
+                    $record['data'][$k]['role_type'] = 1;
+                }else{
+                    $record['data'][$k]['role_type'] = 0;
+                }
+            }
         }
         return $record;
     }
