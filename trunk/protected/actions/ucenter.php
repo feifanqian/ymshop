5c2019b796c0a7a784a2024252d09435bc4ef06e
qianfeifan <893645913@qq.com>
2017-12-21 20:46:06 +0800

    commit

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -2368,7 +2368,13 @@ class UcenterAction extends Controller {
       $idcard = Req::args('idcard');
       $realname = Filter::str(Req::args('realname'));
       $result = Common::aliyunRequest($bankcard,$idcard,$realname);
+      var_dump($result);die;
       if($result['error_code']==0){
+        $has_bind = $this->model->table('bankcard')->where('cardno='.$bankcard)->find();
+        if($has_bind){
+            $this->code = 1191;
+            return;
+        }
         $data = array(
             'user_id'=>$this->user['id'],
             'cardno'=>$bankcard,
