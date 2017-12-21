54814715390743d1a678bdbe11e53ae5df217bb5
qianfeifan <893645913@qq.com>
2017-12-21 15:32:32 +0800

    commit

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -2231,21 +2231,18 @@ class UcenterAction extends Controller {
     public function getSubordinate(){
         // $this->code = 0;
         // $this->content = $this->hirer->getMySubordinate();
-        var_dump($this->user['id']);die;
         $page = Filter::int(Req::args('page'));
         $district = $this->model->table('district_shop')->where('owner_id='.$this->user['id'])->find();
         if(!$district){
             $this->code = 1131;
             return;
         }
-        var_dump(111);
         $record = $this->model->table('invite as do')
                 ->join('left join user as u on do.invite_user_id = u.id')
                 ->fields('u.id,u.avatar,u.nickname,u.sex,do.createtime')
                 ->where("do.user_id=".$this->user['id'])
                 ->order("do.id desc")
-                ->findPage($page, 10);
-        var_dump(222);die;        
+                ->findPage($page, 10);      
         if (empty($record)) {
             return array('data'=>array());
         }
