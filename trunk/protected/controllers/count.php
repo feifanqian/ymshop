e1aa5457f49a2072192ffbcf04d04fbbb578da43
qianfeifan <893645913@qq.com>
2017-12-23 17:14:54 +0800

    commit

--- trunk/protected/controllers/count.php
+++ trunk/protected/controllers/count.php
@@ -752,13 +752,14 @@ class CountController extends Controller
         // $result = $model->query("select *,sum(gnum) as total_num from(select g.store_nums as gnum,g.id as gid,s.name as sname,g.name as gname from tiny_goods as g left join tiny_shop as s on g.shop_id=s.id where g.is_online=0 group by s.id) shops");
         echo "<pre>";
         $result = array_merge($result1,$result2);
-        
-        $res = array();
-        foreach($result as $k=>$v){
-            $res[$v['shid']][] = $v;  
+        $group = array();
+        foreach ($result as $k=>$v) {
+            $group[] = $v['shid'];
         }
         
-        print_r($res);
+        array_multisort($group, SORT_DESC, $result);
+        
+        print_r($result);
         die();
 
         foreach ($result as $k => $v) {
