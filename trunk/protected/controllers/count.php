e34c99fda74b75d9c78999a544184b170d6d0053
qianfeifan <893645913@qq.com>
2017-12-23 16:32:04 +0800

    commit

--- trunk/protected/controllers/count.php
+++ trunk/protected/controllers/count.php
@@ -738,12 +738,15 @@ class CountController extends Controller
         $goods = new Model("goods as gd");
         $shop = new Model("shop as sh");
        // $result = $goods->join("left join shop as sh on gd.shop_id = sh.id")
-       //     ->fields("gd.id as gid,sh.id as shid,sh.name as shname,gd.name as gdname,sell_price,gd.weight as gweight,gd.store_nums as total,cost_price")
+       //     ->fields("gd.id as gid,sh.id as shid,sh.name as shname,gd.name as gdname,gd.sell_price as sprice,gd.weight as gweight,gd.store_nums as total,gd.cost_price as cprice")
        //     ->where('gd.is_online=0')
        //     ->order('sh.id desc')
        //     ->findAll();
         
-        $result = $goods->fields('gd.id as gid,sh.id as shid,sh.name as shname,gd.name as gdname,sell_price,gd.weight as gweight,sum(store_nums) as total,cost_price')->join("left join shop as sh on gd.shop_id = sh.id")->where('gd.is_online=0')->group('sh.id')->findAll();
+        $result = $goods->fields('gd.id as gid,sh.id as shid,sh.name as shname,sum(sell_price) as sprice,sum(gd.weight) as gweight,sum(store_nums) as total,sum(cost_price) as cprice')->join("left join shop as sh on gd.shop_id = sh.id")->where('gd.is_online=0')->group('sh.id')->findAll();
+        foreach($result as $k=>$v){
+            $result[$k]['gdname'] = '';
+        }
            
         $model = new Model();
         // $result = $model->query("select *,sum(gnum) as total_num from(select g.store_nums as gnum,g.id as gid,s.name as sname,g.name as gname from tiny_goods as g left join tiny_shop as s on g.shop_id=s.id where g.is_online=0 group by s.id) shops");
