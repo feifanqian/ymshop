a5bf4233944d6a39769524a3175dbc63c7c28c6b
qianfeifan <893645913@qq.com>
2018-01-02 18:42:03 +0800

    commit

--- trunk/protected/controllers/order.php
+++ trunk/protected/controllers/order.php
@@ -832,6 +832,11 @@ class OrderController extends Controller {
             $where = '1=1';
         } 
             $items = $model->fields("dr.id,dr.doc_type,py.pay_name,dr.amount,us.name,od.type,od.order_no,dr.create_time,dr.payment_time,od.pay_status as pay_status")->join("left join user as us on dr.user_id = us.id left join payment as py on dr.payment_id = py.id left join order as od on dr.order_id = od.id")->where($where)->findAll();
+            if($items){
+                foreach($items as $k=>$v){
+                    $v['order_no'] .='';
+                }
+            }
             if ($items) {
                 header("Content-type:application/vnd.ms-excel");
                 header("Content-Disposition:filename=doc_receiving_list.xls");
