ca56d064ea3d7ce89ff2099b78d722808c263202
qianfeifan <893645913@qq.com>
2018-01-05 14:28:07 +0800

    commit

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -3310,6 +3310,12 @@ class UcenterController extends Controller
             if($has_bind){
                 exit(json_encode(array('status'=>'fail','msg'=>'该银行卡已绑定了')));
             }
+            $bank_code = $result['result']['information']['abbreviation'];
+            if($bank_code){
+                $logo = 'https://apimg.alipay.com/combo.png?d=cashier&t='.$bank_code;
+            }else{
+                $logo = '';
+            }
             $data = array(
                 'user_id'=>$this->user['id'],
                 'cardno'=>$bankcard,
@@ -3318,7 +3324,8 @@ class UcenterController extends Controller
                 'province'=>$province,
                 'city'=>$city,
                 'type'=>intval($result['result']['information']['iscreditcard']),
-                'bank_code'=>$result['result']['information']['abbreviation'],
+                'bank_code'=>$bank_code,
+                'logo'=>$logo,
                 'bind_date'=>date('Y-m-d H:i:s')
                 );
             $this->model->table('bankcard')->data($data)->insert();
