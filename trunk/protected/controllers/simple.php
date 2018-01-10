d85885094f22d98e361b72f1d60b1c62c714e6b6
qianfeifan <893645913@qq.com>
2018-01-10 15:50:19 +0800

    commit

--- trunk/protected/controllers/simple.php
+++ trunk/protected/controllers/simple.php
@@ -1731,7 +1731,7 @@ class SimpleController extends Controller {
         if ($this->checkOnline()) {
             $order_id = Filter::int(Req::get("order_id"));
             if ($order_id) {
-                $order = $this->model->table("order as od")->join("left join payment as pa on od.payment= pa.id")->fields("od.id,od.order_no,od.payment,od.pay_status,od.order_amount,od,create_time,pa.pay_name as payname,od.type,od.status")->where("od.id=$order_id and od.status<4 and od.user_id = " . $this->user['id'])->find();
+                $order = $this->model->table("order as od")->join("left join payment as pa on od.payment= pa.id")->fields("od.id,od.order_no,od.payment,od.pay_status,od.order_amount,od.create_time,pa.pay_name as payname,od.type,od.status")->where("od.id=$order_id and od.status<4 and od.user_id = " . $this->user['id'])->find();
                 if ($order) {
                     if ($order['pay_status'] == 0) {
                         $payment_plugin = Common::getPaymentInfo($order['payment']);
