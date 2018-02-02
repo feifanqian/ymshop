6a817391d345c0f466b92653c7645b076b9916bf
qianfeifan <893645913@qq.com>
2018-02-02 16:06:53 +0800

    commit

--- trunk/framework/web/controller/controller_class.php
+++ trunk/framework/web/controller/controller_class.php
@@ -473,7 +473,7 @@ class Controller extends Object {
             $actionId = $this->defaultAction;
         //统一拦截权限控制
         if ($this->checkRight($id) == false) {
-            var_dump($actionId);die;
+            var_dump($this->defaultAction);die;
             $this->noRight();
         } else {
             //如果控制器直接定义了方式
