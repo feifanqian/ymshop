6dafc880175a8a980d642d2732734336e83a2863
qianfeifan <893645913@qq.com>
2018-04-02 18:54:54 +0800

    commit

--- trunk/protected/controllers/payment.php
+++ trunk/protected/controllers/payment.php
@@ -968,10 +968,10 @@ class PaymentController extends Controller {
         } else {
             $sendData = $_GET;
             unset($sendData['con'], $sendData['act']);
-            echo "<pre>";
-            print_r($sendData);
-            echo "<pre>";
-            die;
+            // echo "<pre>";
+            // print_r($sendData);
+            // echo "<pre>";
+            // die;
             $this->assign("sendData", $sendData);
             $this->redirect();
         }
