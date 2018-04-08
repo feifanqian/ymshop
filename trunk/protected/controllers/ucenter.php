ec499daf61408455f4049340448e9f90ff9dbacf
qianfeifan <893645913@qq.com>
2018-04-08 16:00:55 +0800

    commit

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -3734,7 +3734,7 @@ class UcenterController extends Controller
             if($this->user['id']==42608){    
                 $data = array(
                     'picType'=>'00',
-                    'picFile'=>$info1,
+                    'picFile'=>$info1[0],
                     'token'=>$ret['ysepay_merchant_register_token_get_response']['token'],
                     'superUsercode'=>'yuanmeng'
                     );
