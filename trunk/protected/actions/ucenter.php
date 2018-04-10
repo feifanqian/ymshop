044a95ab4d9908ae6b522ad4441a429959c1f4c9
qianfeifan <893645913@qq.com>
2018-04-10 15:16:36 +0800

    commit

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -2859,7 +2859,7 @@ class UcenterAction extends Controller {
         // var_dump($signStr);
         $sign = $this->sign_encrypt(array('data' => $signStr));
         $myParams['sign'] = trim($sign['check']);
-        // var_dump($myParams);
+        var_dump($myParams);
         $act = "https://commonapi.ysepay.com/gateway.do";
         $result = Common::httpRequest($act,'POST',$myParams);
         var_dump($result);die;
