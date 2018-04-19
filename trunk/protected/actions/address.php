8986323db866243f714b3ea63b3910d7e207d477
qianfeifan <893645913@qq.com>
2018-04-19 17:15:14 +0800

    commit

--- trunk/protected/actions/address.php
+++ trunk/protected/actions/address.php
@@ -1087,7 +1087,7 @@ class AddressAction extends Controller
     public function get_all_child_promoters(){      
         $user_id = Filter::int(Req::args('user_id'));
         $shop = $this->model->table('district_shop')->fields('id')->where('owner_id='.$user_id)->find();
-        $list = $this->model->table('district_promoter')->fields('dp.user_id')->join('LEFT JOIN customer AS c ON dp.user_id = c.user_id LEFT JOIN district_shop AS ds ON dp.hirer_id = ds.id')->where('ds.invite_shop_id ='.$shop['id'])->findAll();
+        $list = $this->model->table('district_promoter as dp')->fields('dp.user_id')->join('LEFT JOIN customer AS c ON dp.user_id = c.user_id LEFT JOIN district_shop AS ds ON dp.hirer_id = ds.id')->where('ds.invite_shop_id ='.$shop['id'])->findAll();
 
         // var_dump($user_id);die;
         // $result = Common::getAllChildPromoters($user_id);
