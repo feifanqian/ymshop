1178db97c8f6377362cf4934368700e6b3cd132d
qianfeifan <893645913@qq.com>
2018-04-20 10:35:45 +0800

    commit

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -2843,7 +2843,8 @@ class UcenterAction extends Controller {
         $biz_content_arr = array(
             "out_trade_no" => $order_no,
             'payee_usercode' => 'yuanmeng',
-            "org_no" => "6584000000",
+            // "org_no" => "6584000000",
+            "org_no" => "",
             "division_mode" => "01",
             "total_amount" => $order['order_amount'],
             "is_divistion" => "01",
