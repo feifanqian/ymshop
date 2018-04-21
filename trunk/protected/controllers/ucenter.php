0c1b9a49bbf87c01febc828b9e2164594f636523
qianfeifan <893645913@qq.com>
2018-04-21 18:47:44 +0800

    commit

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -131,6 +131,7 @@ class UcenterController extends Controller
                 exit;
             }else{
                 $auth_code = $_GET['auth_code'];
+                var_dump($auth_code);
                 $pay_alipayapp = new pay_alipayapp();
                 $result = $pay_alipayapp->alipayLogin($auth_code);
                 var_dump($result);die;
