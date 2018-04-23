6f5c1f8dc11d015c085a22b108f70d25ef604f60
qianfeifan <893645913@qq.com>
2018-04-23 10:17:49 +0800

    commit

--- trunk/protected/classes/Common.php
+++ trunk/protected/classes/Common.php
@@ -1572,4 +1572,15 @@ class Common {
         array_multisort($arrSort[$field], constant($sort), $array);
         return $array;
     } 
+
+    static function std_class_object_to_array($stdclassobject)
+    {
+    　　$_array = is_object($stdclassobject) ? get_object_vars($stdclassobject) : $stdclassobject;
+    　　foreach ($_array as $key => $value) {
+    　　　　$value = (is_array($value) || is_object($value)) ? std_class_object_to_array($value) : $value;
+    　　　　$array[$key] = $value;
+    　　}
+
+    　　return $array;
+    }
 }
