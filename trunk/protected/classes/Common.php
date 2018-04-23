45a0cd073f89e955b2122210df6e9b9bc53d7dca
qianfeifan <893645913@qq.com>
2018-04-23 10:23:47 +0800

    commit

--- trunk/protected/classes/Common.php
+++ trunk/protected/classes/Common.php
@@ -1573,14 +1573,14 @@ class Common {
         return $array;
     } 
 
-    static function std_class_object_to_array($stdclassobject)
-    {
-    　　$_array = is_object($stdclassobject) ? get_object_vars($stdclassobject) : $stdclassobject;
-    　　foreach ($_array as $key => $value) {
-    　　　　$value = (is_array($value) || is_object($value)) ? std_class_object_to_array($value) : $value;
-    　　　　$array[$key] = $value;
-    　　}
+    // static function std_class_object_to_array($stdclassobject)
+    // {
+    // 　　$_array = is_object($stdclassobject) ? get_object_vars($stdclassobject):$stdclassobject;
+    // 　　foreach ($_array as $key => $value) {
+    // 　　　　$value = (is_array($value) || is_object($value)) ? std_class_object_to_array($value):$value;
+    // 　　　　$array[$key] = $value;
+    // 　　}
 
-    　　return $array;
-    }
+    // 　　return $array;
+    // }
 }
