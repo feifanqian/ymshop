485d3c86f7a418eafc3414284b0faa5ff03c72ad
qianfeifan <893645913@qq.com>
2018-04-23 11:06:01 +0800

    commit

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -155,7 +155,7 @@ class UcenterController extends Controller
             $pay_alipayapp = new pay_alipayapp();
             $result = $pay_alipayapp->alipayLogin($auth_code);
             $result= Common::objectToArray($result);
-            $result['alipay_system_oauth_token_response'] = (object)$result['alipay_system_oauth_token_response'];
+            $result['alipay_system_oauth_token_response'] = (array)$result['alipay_system_oauth_token_response'];
             echo "<pre>";
             print_r($result);
             echo "<pre>";
