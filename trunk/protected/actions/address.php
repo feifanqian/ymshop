d2d3197c7dc7c42e0b3c078b018bc1f508aef04e
qianfeifan <893645913@qq.com>
2018-04-26 16:20:18 +0800

    commit

--- trunk/protected/actions/address.php
+++ trunk/protected/actions/address.php
@@ -911,7 +911,7 @@ class AddressAction extends Controller
         //     $order = 'dist asc';
         // }
 
-        $info_sql = $this->model->table('district_promoter')->fields("id,user_id,shop_name,type,status,base_rate,location,province_id,city_id,region_id,road,lng,lat,picture,info,classify_id,hot,evaluate,taste,environment,quality_service,price,shop_type,(6378.138 * 2 * asin(sqrt(pow(sin((lat * pi() / 180 - ".$lat." * pi() / 180) / 2),2) + cos(lat * pi() / 180) * cos(".$lat." * pi() / 180) * pow(sin((lng * pi() / 180 - ".$lng." * pi() / 180) / 2),2))) * 1000) as distance")->where($where)->order($order)->findPage($page, 10);
+        $info_sql = $this->model->table('district_promoter')->fields("id,user_id,shop_name,type,status,base_rate,location,province_id,city_id,region_id,road,lng,lat,picture,info,classify_id,hot,evaluate,taste,environment,quality_service,price,shop_type,(6378.138 * 2 * asin(sqrt(pow(sin((lat * pi() / 180 - ".$lat." * pi() / 180) / 2),2) + cos(lat * pi() / 180) * cos(".$lat." * pi() / 180) * pow(sin((lng * pi() / 180 - ".$lng." * pi() / 180) / 2),2)))) as dist")->where($where)->order($order)->findPage($page, 10);
         if(!$info_sql){
             $this->code = 0;
             $this->content = [];
@@ -993,7 +993,7 @@ class AddressAction extends Controller
                         unset($info_sql[$key]);
                     }
                 }
-            $info_sql[$key]['dist'] = Common::getDistanceByLatLng($lat,$lng,$value['lat'],$value['lng'])/1000;
+            // $info_sql[$key]['dist'] = Common::getDistanceByLatLng($lat,$lng,$value['lat'],$value['lng'])/1000;
             // $arr[] = $info_sql[$key]['dist'];
             
             // if($info_sql[$key]['dist']>$radius && empty($tourist_id) && empty($distance)){
