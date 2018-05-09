992edc222948f4fed2000dbc107e88630adc62fc
qianfeifan <893645913@qq.com>
2018-05-09 19:03:13 +0800

    test

--- trunk/protected/actions/goods.php
+++ trunk/protected/actions/goods.php
@@ -43,14 +43,14 @@ class GoodsAction extends Controller {
         $req->setFields("num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick");
         $req->setQ("女装");
         $req->setCat("16,18");
-        $req->setItemloc("杭州");
+        // $req->setItemloc("杭州");
         $req->setSort("tk_rate_des");
         $req->setIsTmall("false");
         $req->setIsOverseas("false");
         $req->setStartPrice("10");
-        $req->setEndPrice("10");
-        $req->setStartTkRate("123");
-        $req->setEndTkRate("123");
+        $req->setEndPrice("500");
+        // $req->setStartTkRate("123");
+        // $req->setEndTkRate("123");
         $req->setPlatform("1");
         $req->setPageNo("123");
         $req->setPageSize("20");
