33898dd033e56eb9281f41eae6fea90da9a11a67
qianfeifan <893645913@qq.com>
2018-05-22 12:02:38 +0800

    test

---
+++ trunk/protected/actions/cashier.php
@@ -0,0 +1,73 @@
+<?php
+class CashierAction extends Controller
+{
+    public $model = null;
+    public $user = null;
+    public $code = 1000;
+    public $content = NULL;
+
+    public function __construct()
+    {
+        $this->model = new Model();
+    }
+    
+    //邀请收银员
+    public function add_cashier()
+    {
+    	$mobile = Filter::sql(Req::args('mobile'));
+    	if (!Validator::mobi($mobile)) {
+            $this->code = 1024;
+            return;
+        }
+        $job_no = Filter::sql(Req::args('job_no'));
+        if(!$job_no) {
+        	$this->code = 1239;
+            return;
+        }
+        $job_no_exist = $this->model->table('cashier')->where('job_no='.$job_no.' and hire_user_id='.$this->user['id'])->find();
+        if($job_no_exist) {
+        	$this->code = 1240;
+            return;
+        }
+        $cashier = $this->model->table('customer')->fields('user_id')->where('mobile='.$mobile)->find();
+        if(!$cashier) {
+        	$this->code = 1159;
+            return;
+        }
+        $promoter = $this->model->table('district_promoter as dp')->fields('dp.id,dp.user_id,c.real_name')join("customer AS c ON dp.user_id=c.user_id")->where('dp.user_id='.$this->user['id'])->find();
+        if(!$promoter) {
+        	$this->code = 1159;
+            return;
+        }
+        $data = array(
+        	'user_id'=>$cashier['user_id'],
+        	'hire_promoter_id'=>$promoter['id'],
+        	'hire_user_id'=>$this->user['id'],
+        	'mobile'=>$mobile,
+        	'status'=>0
+        	);
+        $res = $this->model->table('cashier')->data($data)->insert();
+        $type = 'cashier_invite';
+        $content = "收到一条来自于商家{$promoter['real_name']}的成为收银员邀请";
+        $platform = 'all';
+        if (!$this->jpush) {
+            $NoticeService = new NoticeService();
+            $this->jpush = $NoticeService->getNotice('jpush');
+        }
+        $audience['alias'] = array($cashier['user_id']);
+        $this->jpush->setPushData($platform, $audience, $content, $type, "");
+        $ret = $this->jpush->push();
+        if(!$ret) {
+        	$this->code = 1242;
+            return;
+        }
+        if($res) {
+        	$this->code = 0;
+            return;
+        } else {
+            $this->code = 1241;
+            return;
+        } 
+    }
+}
+?>
