bc92a3ed4b26f2f5e3eeef183af559475b8acd40
qianfeifan <893645913@qq.com>
2018-06-04 15:31:22 +0800

    test

--- trunk/protected/actions/goods.php
+++ trunk/protected/actions/goods.php
@@ -323,6 +323,6 @@ class GoodsAction extends Controller {
         $t1 = mb_strpos($str,$begin);
         $t2 = mb_strpos($str,$end);
         $ret = mb_substr($str,$t1,$t2-$t1);
-        return $ret;
+        return substr($ret,1);
     }
 }
