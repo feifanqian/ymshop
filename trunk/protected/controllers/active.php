1968270b40a502656b92362b8d09d55f6fdba840
qianfeifan <893645913@qq.com>
2018-06-09 10:05:59 +0800

    test

--- trunk/protected/controllers/active.php
+++ trunk/protected/controllers/active.php
@@ -36,5 +36,57 @@ class ActiveController extends Controller
     public function login() {
     	$this->redirect();
     }
+
+    public function login_act() {
+        $redirectURL = Req::args("redirectURL");
+        $this->assign("redirectURL", $redirectURL);
+        $account = Filter::sql(Req::post('account'));
+        $passWord = Req::post('password');
+        $autologin = Req::args("autologin");
+        if ($autologin == null)
+            $autologin = 0;
+        $model = $this->model->table("user as us");
+        $obj = $model->join("left join customer as cu on us.id = cu.user_id")->fields("us.*,cu.group_id,cu.user_id,cu.login_time,cu.mobile,cu.real_name")->where("us.email='$account' or us.name='$account' or cu.mobile='$account' and cu.status=1")->find();
+        if ($obj) {
+            if ($obj['status'] == 1) {
+                if ($obj['password'] == CHash::md5($passWord, $obj['validcode'])) {
+                    $cookie = new Cookie();
+                    $cookie->setSafeCode(Tiny::app()->getSafeCode());
+                    if ($autologin == 1) {
+                        $this->safebox->set('user', $obj, $this->cookie_time);
+                        $cookie->set('autologin', array('account' => $account, 'password' => $obj['password']), $this->cookie_time);
+                    } else {
+                        $cookie->set('autologin', null, 0);
+                        $this->safebox->set('user', $obj, 1800);
+                    }
+                    $this->model->table("customer")->data(array('login_time' => date('Y-m-d H:i:s')))->where('user_id=' . $obj['id'])->update();
+                    $redirectURL = Req::args("redirectURL");
+
+                    if ($redirectURL != '' && preg_match("/https?:\/\//i", $redirectURL) == 0 && stripos($redirectURL, "reg") === false && stripos($redirectURL, "login_act") === false && stripos($redirectURL, "oauth_bind") === false && stripos($redirectURL, "activation_user") === false && stripos($redirectURL, "reset_password_act") === false)
+                        header('Location: ' . $redirectURL, true, 302);
+                    else
+                        $url = Cookie::get('url');
+                        $url = $url!=NULL?$url:'/ucenter/index';
+                        if(strpos($url, '/')!==0){
+                            $url = "/".$url;
+                        }
+                        header("Location:$url");
+                    exit;
+                }else {
+                    $info = array('field' => 'password', 'msg' => '密码错误！');
+                }
+            } else if ($obj['status'] == 2) {
+                $info = array('field' => 'account', 'msg' => '账号已经锁定，请联系管理人员！');
+            } else {
+                $info = array('field' => 'account', 'msg' => '账号还未激活，无法登录！');
+            }
+        } else {
+            $info = array('field' => 'account', 'msg' => '账号不存在！');
+        }
+        $this->assign("invalid", $info);
+        $this->redirect("login", false, Req::args());
+    }
+
+    
 }
 ?>
