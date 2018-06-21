450ae4916b3006180c13ee252cbb164312c41447
qianfeifan <893645913@qq.com>
2018-06-21 09:48:47 +0800

    test

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -866,7 +866,7 @@ class UcenterAction extends Controller {
     }
 
     public function push_message() {
-        $list = $this->model->table("push_message")->where("to_id=" . $this->user['id'])->findAll();
+        $list = $this->model->table("push_message as p")->fields('p.to_id,p.type,p.content,p.create_time,p.value,c.status')->join('left join cashier as c on p.value=c.id')->where("to_id=" . $this->user['id'])->findAll();
         $this->code = 0;
         $this->content = $list;
     }
