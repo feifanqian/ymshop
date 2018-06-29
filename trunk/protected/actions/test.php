f33c45542f15efe3e7feab16e9ceaf32aef3d080
qianfeifan <893645913@qq.com>
2018-06-29 10:39:31 +0800

    test

--- trunk/protected/actions/test.php
+++ trunk/protected/actions/test.php
@@ -14,8 +14,11 @@ class TestAction extends Controller
 
     public function test() {   		
     	$taobao_pid = $this->model->table('taoke_pid')->where('user_id is NULL')->order('id desc')->find();
+        if($taobao_pid) {
+            $this->model->table('taoke_pid')->data(['user_id'=>42608])->where('id='.$taobao_pid['id'])->update();
+        }
     	$this->code = 0;
-    	$this->content = $taobao_pid;
+    	// $this->content = $taobao_pid;
         // $this->time = time();
         return;
     }
