ec5244187a7c0f53622693c3b592197731a6f17e
qianfeifan <893645913@qq.com>
2018-07-04 18:18:35 +0800

    test

--- trunk/protected/actions/cashier.php
+++ trunk/protected/actions/cashier.php
@@ -879,6 +879,7 @@ class CashierAction extends Controller
                 $where.=" and order_status in ('订单失效')";
                 break;    
         }
+        var_dump($where);die;
         $list = $this->model->table('taoke')->fields('id,order_sn,goods_name,order_amount,effect_prediction,create_time,order_status')->where($where)->findPage($page,10);
         if($list) {
             if(isset($list['data']) && $list['data']!=null) {
