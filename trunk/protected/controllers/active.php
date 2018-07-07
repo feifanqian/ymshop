97d9c24b55cf3ce1e4617a9aabeac9acdc5cae04
qianfeifan <893645913@qq.com>
2018-07-07 15:17:58 +0800

    test

--- trunk/protected/controllers/active.php
+++ trunk/protected/controllers/active.php
@@ -548,10 +548,10 @@ class ActiveController extends Controller
             'code' => $code,
         ));
         $flag = json_decode($return, true);
-        var_dump($flag);die;
         if ($flag['status'] == 200) {
             return true;
         } else {
+            var_dump($flag);die;
             return false;
         }
     }
