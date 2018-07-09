a266247283d70b50b5a1632cdf40374e01f67b51
qianfeifan <893645913@qq.com>
2018-07-09 21:10:05 +0800

    test

--- trunk/protected/controllers/travel.php
+++ trunk/protected/controllers/travel.php
@@ -136,7 +136,7 @@ class TravelController extends Controller
         if(!$this->user['id']) {
             $this->redirect('/active/login?redirect=pay&id={$id}');
         }
-        var_dump(111);
+        var_dump(111);die;
         $order = $this->model->table('travel_order as t')->fields('t.id,t.order_no,tw.name,tw.city,tw.date,tw.desc,t.order_amount,tw.img,tw.price,t.way_id,t.contact_name,t.contact_phone,t.id_no,t.idcard_url,t.sex,t.pay_status,t.pay_type')->join('left join travel_way as tw on t.way_id=tw.id')->where('t.id='.$id)->find();
         if(!$order) {
             $this->redirect("/index/msg", false, array('type' => "fail", "msg" => '支付信息错误', "content" => "抱歉，找不到您的订单信息啦"));
@@ -161,7 +161,7 @@ class TravelController extends Controller
                     'open_id' => $token['openid']
                 ))->insert();
         }
-        var_dump(222);
+        var_dump(222);die;
         if($oauth_user) {
             // $openid = $oauth->getOpenid($code);
             $openid = $oauth_user['open_id'];
@@ -183,7 +183,7 @@ class TravelController extends Controller
             $jsApiParameters = $tools->GetJsApiParameters($order_input);
             $this->assign("jsApiParameters", $jsApiParameters);
         }
-        var_dump(333);
+        var_dump(333);die;
         $success_url = Url::urlFormat("/travel/order_detail/id/{$id}");
         $this->assign("need_code", $need_code);
         $this->assign("success_url", $success_url);
