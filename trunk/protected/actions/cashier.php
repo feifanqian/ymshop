347364ba00ccc697f3a8d37f244bc7f2b9b1625d
qianfeifan <893645913@qq.com>
2018-07-13 15:03:07 +0800

    test

--- trunk/protected/actions/cashier.php
+++ trunk/protected/actions/cashier.php
@@ -645,7 +645,7 @@ class CashierAction extends Controller
     public function voucher_list() {
         $user_id = $this->user['id'];
         $page = Filter::int(Req::args('p'));
-        $list = $this->model->table('active_voucher')->where('status=1 and user_id='.$user_id)->findPage($page,10);
+        $list = $this->model->table('active_voucher')->where('status=1 and user_id='.$user_id)->order('create_time desc')->findPage($page,10);
         if($list) {
             if(isset($list['data']) && $list['data']!=null) {
                 foreach ($list['data'] as $k => $v) {
