a4a9b5a164af32148f23917b70aa01a54d58aacc
qianfeifan <893645913@qq.com>
2018-07-20 14:06:36 +0800

    test

--- trunk/protected/actions/groupbuy.php
+++ trunk/protected/actions/groupbuy.php
@@ -126,7 +126,7 @@ class GroupbuyAction extends Controller
         $info['current_time'] = date('Y-m-d H:i:s');
         $info['join_num'] = $this->model->table('groupbuy_log')->where('groupbuy_id='.$groupbuy_id.' and pay_status=1')->count();
         // $info['groupbuy_join_list'] = $this->model->table('groupbuy_join')->fields('id as join_id,user_id,need_num,end_time')->where('groupbuy_id='.$groupbuy_id.' and status in (0,1) and pay_status=1')->findAll();
-        $info['groupbuy_join_list'] = $this->model->table('groupbuy_log as gl')->fields('gl.join_id,gj.user_id,gj.need_num,gj.end_time')->join('left join groupbuy_join as gj on gl.join_id=gj.id')->where('gl.groupbuy_id='.$groupbuy_id.' and gl.pay_status=1')->findAll();
+        $info['groupbuy_join_list'] = $this->model->table('groupbuy_log as gl')->fields('gl.join_id,gj.user_id,gj.need_num,gj.end_time')->join('left join groupbuy_join as gj on gl.join_id=gj.id')->where('gl.groupbuy_id='.$groupbuy_id.' and gl.pay_status=1 and gj.need_num>0')->findAll();
         
         if($info['groupbuy_join_list']) {
             foreach ($info['groupbuy_join_list'] as $k => $v) {
