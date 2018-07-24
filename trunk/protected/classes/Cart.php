4b3db125dc77d2a06aea27040593e4a6d9477bdc
qianfeifan <893645913@qq.com>
2018-07-24 20:45:35 +0800

    test

--- trunk/protected/classes/Cart.php
+++ trunk/protected/classes/Cart.php
@@ -104,9 +104,6 @@ class Cart {
     }
 
     public function all($uid=0) {
-        if($uid==42608) {
-        var_dump(222);die;
-        }
         $products = array();
         if ($this->getCnt() > 0) {
             $model = new Model("products as pr");
