291ff44c32a24a823d13780fe6dbd0dc09ec4690
qianfeifan <893645913@qq.com>
2018-07-25 19:25:55 +0800

    test

--- trunk/protected/classes/Cart.php
+++ trunk/protected/classes/Cart.php
@@ -231,7 +231,7 @@ class Cart {
             if ($idstr != '') {
                 $prom = new Prom();
                 $items = $model->fields("pr.*,go.img,go.name,go.prom_id,go.point,go.freeshipping,go.shop_id,c.num")->join("left join goods as go on pr.goods_id = go.id left join cart as c on pr.goods_id=c.goods_id")->where("pr.id in($idstr)")->findAll();  
-                foreach ($items as $item) {
+                foreach ($items as $k=>$item) {
                     $num = $item['num'];
                     if ($num > $item['store_nums']) {
                         $num = $item['store_nums'];
@@ -245,7 +245,7 @@ class Cart {
                         $prom_goods = $prom->prom_goods($item);
                         $amount = sprintf("%01.2f", $prom_goods['real_price'] * $num);
                         $sell_total = $item['sell_price'] * $num;
-                        $products[$item['id']] = array('id' => $item['id'], 'goods_id' => $item['goods_id'], 'shop_id' => $item['shop_id'], 'name' => $item['name'], 'img' => $item['img'], 'num' => $num, 'store_nums' => $item['store_nums'], 'price' => $item['sell_price'], 'freeshipping'=>$item['freeshipping'], 'prom_id' => $item['prom_id'], 'real_price' => $prom_goods['real_price'], 'sell_price' => $item['sell_price'], 'spec' => unserialize($item['spec']), 'amount' => $amount, 'prom' => $prom_goods['note'], 'weight' => $item['weight'], 'point' => $item['point'], 'sell_total' => $sell_total, "prom_goods" => $prom_goods);
+                        $products[$k] = array('id' => $item['id'], 'goods_id' => $item['goods_id'], 'shop_id' => $item['shop_id'], 'name' => $item['name'], 'img' => $item['img'], 'num' => $num, 'store_nums' => $item['store_nums'], 'price' => $item['sell_price'], 'freeshipping'=>$item['freeshipping'], 'prom_id' => $item['prom_id'], 'real_price' => $prom_goods['real_price'], 'sell_price' => $item['sell_price'], 'spec' => unserialize($item['spec']), 'amount' => $amount, 'prom' => $prom_goods['note'], 'weight' => $item['weight'], 'point' => $item['point'], 'sell_total' => $sell_total, "prom_goods" => $prom_goods);
                         // array_push($products,$products[$item['id']]);
                     }
                 }
