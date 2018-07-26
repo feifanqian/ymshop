05f88f027f74dc1759a78cbb8df0f751f51d0bd6
qianfeifan <893645913@qq.com>
2018-07-26 15:31:22 +0800

    test

--- trunk/auto.php
+++ trunk/auto.php
@@ -336,13 +336,13 @@ class LinuxCliTask{
     public function autoBackGroupbuyMoney()
     {
         $order = $this->model->table('order')->where('pay_status=1 and type=1 and delivery_status=0 and status!=4')->findAll();
-        var_dump($order);die;
         if($order) {
             foreach($order as $k=>$v) {
                 $now = time();
                 $amount = $v['order_amount']; 
                 if($v['join_id']!=0) {
                     $groupbuy_log = $this->model->table('groupbuy_log')->fields('id,join_id,user_id,join_time')->where('id='.$v['join_id'].' and pay_status=1')->findAll();
+                    var_dump($groupbuy_log);die;
                     if($groupbuy_log) {
                         foreach ($groupbuy_log as $key => $value) {
                             $groupbuy_join = $this->model->table('groupbuy_join')->where('id='.$value['join_id'])->find();
