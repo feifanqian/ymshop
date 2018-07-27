ce211cf3af63b9c090d4e5aabdb249be751fcbbe
qianfeifan <893645913@qq.com>
2018-07-27 10:48:47 +0800

    test

--- trunk/protected/controllers/marketing.php
+++ trunk/protected/controllers/marketing.php
@@ -1101,8 +1101,8 @@ class MarketingController extends Controller {
         if(!$page) {
             $page = 1;
         }
-
-        $list = $this->model->table('order as o')->fields('gl.id as log_id,gl.join_id,gl.groupbuy_id as id,go.name,go.img,g.min_num,g.price,gj.end_time,gj.status,o.id as order_id,og.product_id,gl.status as gl_status,u.nickname,g.title,gl.join_time')->join('left join groupbuy_log as gl on o.join_id=gl.id left join order_goods as og on o.id=og.order_id left join groupbuy as g on gl.groupbuy_id=g.id left join goods as go on g.goods_id=go.id left join groupbuy_join as gj on gl.join_id=gj.id left join user as u on o.user_id=u.id')->where('gl.pay_status in (1,3) and o.pay_status in (1,3)')->order('id desc')->findPage($page,10);
+        $model = new Model();
+        $list = $model->table('order as o')->fields('gl.id as log_id,gl.join_id,gl.groupbuy_id as id,go.name,go.img,g.min_num,g.price,gj.end_time,gj.status,o.id as order_id,og.product_id,gl.status as gl_status,u.nickname,g.title,gl.join_time')->join('left join groupbuy_log as gl on o.join_id=gl.id left join order_goods as og on o.id=og.order_id left join groupbuy as g on gl.groupbuy_id=g.id left join goods as go on g.goods_id=go.id left join groupbuy_join as gj on gl.join_id=gj.id left join user as u on o.user_id=u.id')->where('gl.pay_status in (1,3) and o.pay_status in (1,3)')->order('id desc')->findPage($page,10);
         if($list) {
             if($list['data']!=null) {
                 foreach ($list['data'] as $k => $v) {
