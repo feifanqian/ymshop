fa143f00b0f47536624d802cd0484803ebdf59bd
qianfeifan <893645913@qq.com>
2018-07-27 11:45:18 +0800

    test

--- trunk/protected/actions/order.php
+++ trunk/protected/actions/order.php
@@ -518,7 +518,9 @@ class OrderAction extends Controller {
             $payable_freight = '0.00';
         }
         $real_freight = $payable_freight;
-
+        if($this->user['id']==42608 && $order_type==1) {
+            var_dump($payable_freight);die;
+        }
         //计算订单优惠
         $prom_order = array();
         $discount_amount = 0;
