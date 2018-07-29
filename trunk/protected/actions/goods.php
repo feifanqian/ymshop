e66434183a45f7fbe02fcbc7c9cbb7742d0d5579
jarSimona <hechunmao1990@163.com>
2018-07-29 22:36:38 +0800

    优惠购优化图片

--- trunk/protected/actions/goods.php
+++ trunk/protected/actions/goods.php
@@ -401,6 +401,9 @@ class GoodsAction extends Controller {
                         if ($decrease_price < 5) {
                             continue;
                         }
+                        if(!$itm['small_images']){
+                            $itm['small_images']['string'] = [$itm['pict_url']];
+                        }
                         $itm['decrease_price'] = $decrease_price;
                         $itm['final_price'] = $itm['zk_final_price'] - $itm['decrease_price'];
                         $itm['nick'] = $itm['shop_title'];
