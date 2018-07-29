709bf24b38cce89bfee8586af026718354a7771b
jarSimona <hechunmao1990@163.com>
2018-07-29 18:07:11 +0800

    第三方登录修复

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -322,7 +322,10 @@ class UcenterAction extends Controller {
         $token = Filter::sql(Req::args('token'));
 
         if ($platform && $openid && $token) {
-            $obj = $this->model->table('oauth_user')->where("oauth_type='{$platform}' and open_id='{$openid}'")->find();
+            $model = $this->model->table("oauth_user as ou");
+            $obj = $model->join("left join user as us on us.id = ou.user_id")->fields("ou.*,us.adzoneid")->where("ou.oauth_type='$platform' and ou.open_id='{$openid}'")->find();
+ 
+            // $obj = $this->model->table('oauth_user')->where("oauth_type='{$platform}' and open_id='{$openid}'")->find();
             if ($obj) {
                 $token = CHash::random(32, 'char');
                 $this->model->table("customer")->data(array('login_time' => date('Y-m-d H:i:s')))->where('user_id=' . $obj['user_id'])->update();
