41674b8a2f292e9e18c842af0b700f94ddc0b51c
qianfeifan <893645913@qq.com>
2018-07-29 17:01:31 +0800

    update

--- trunk/protected/actions/ucenter.php
+++ trunk/protected/actions/ucenter.php
@@ -327,6 +327,13 @@ class UcenterAction extends Controller {
                 $token = CHash::random(32, 'char');
                 $this->model->table("customer")->data(array('login_time' => date('Y-m-d H:i:s')))->where('user_id=' . $obj['user_id'])->update();
                 $this->model->table("user")->data(array('token' => $token, 'expire_time' => date('Y-m-d H:i:s', strtotime('+1 day'))))->where('id=' . $obj['user_id'])->update();
+                if($obj['adzoneid']==null) {
+                        $taobao_pid = $this->model->table('taoke_pid')->where('user_id is NULL')->order('id desc')->find();
+                        if($taobao_pid) {
+                            $this->model->table('taoke_pid')->data(['user_id'=>$obj['id']])->where('id='.$taobao_pid['id'])->update();
+                            $this->model->table('user')->data(['adzoneid'=>$taobao_pid['adzoneid']])->where('id='.$obj['id'])->update();
+                        }
+                    }
                 $this->code = 0;
                 $this->content = array(
                     'user_id' => $obj['user_id'],
