acf03bdcc6de3e26f73354f87b3e80eb676a2527
qianfeifan <893645913@qq.com>
2018-08-02 16:03:36 +0800

    test

--- trunk/protected/actions/order.php
+++ trunk/protected/actions/order.php
@@ -1118,7 +1118,7 @@ class OrderAction extends Controller {
         $order_info = $this->model->table("order")->where("id=$order_id")->find();
         if($order_info['type']==1) {
             //判断团购订单是否满足发货要求，需在规定时间满足达到最小组团人数
-            $groupbuy_log = $this->model->table('groupbuy_log')->where('id='.$order_info['join_id'];)->find();
+            $groupbuy_log = $this->model->table('groupbuy_log')->where('id='.$order_info['join_id'])->find();
             if($groupbuy_log) {
                 $groupbuy_join = $this->model->table('groupbuy_join')->where('id='.$groupbuy_log['join_id'])->find();
                 if($groupbuy_join) {
