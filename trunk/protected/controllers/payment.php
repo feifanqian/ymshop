5a460667904e0b14caa440719af90afda1a1ac4f
qianfeifan <893645913@qq.com>
2018-08-09 14:48:03 +0800

    test

--- trunk/protected/controllers/payment.php
+++ trunk/protected/controllers/payment.php
@@ -955,7 +955,8 @@ class PaymentController extends Controller {
             // print_r($myParams);
             // echo "<pre>";
             // die;
-            var_dump($signStr);die;
+            var_dump($signStr);
+            var_dump($ret['ysepay_online_jsapi_pay_response']);die;
             $this->redirect("/index/msg", false, array('type' => 'fail', 'msg' => $ret['ysepay_online_jsapi_pay_response']['sub_msg']));
             exit();
         }
