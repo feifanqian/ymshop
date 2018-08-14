c7dc51ffd499f3ea0e2a7c7e548d35d8c90ee7e1
qianfeifan <893645913@qq.com>
2018-08-14 18:01:27 +0800

    test

--- trunk/protected/actions/map.php
+++ trunk/protected/actions/map.php
@@ -63,4 +63,19 @@ class MapAction extends Controller
         $this->content = $list;
     }
 
+    public function image_merge_test()
+    {
+        // 图片一
+        $path_1 = 'http://www.ymlypt.com/static/images/0001.jpg';
+        $image_1 = imagecreatefrompng($path_1);
+        // 合成图片
+        // imagecopymerge($image_1, $image_2, 0, 0, 0, 0, imagesx($image_2), imagesy($image_2), 100);
+        $black = imagecolorallocate($image_1, 0, 0, 0);
+        $font = '/static/fonts/Dejavusans_0.ttf';
+
+        imagettftext($image_1, 16, 0, imagesx($image_1)-160, imagesy($image_1)-20, $black, $font, 'MKTK-HELOO');
+        // 输出合成图片
+        var_dump(imagepng($image_1, APP_ROOT.'static/images/temp/'.time().'.png'));
+    }
+
 }
