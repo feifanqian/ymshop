44382c4e15d7db43f3712129217cd57349fe8023
qianfeifan <893645913@qq.com>
2018-09-06 12:01:49 +0800

    test

--- trunk/protected/controllers/travel.php
+++ trunk/protected/controllers/travel.php
@@ -12,20 +12,20 @@ class TravelController extends Controller
     public function init() {
         header("Content-type: text/html; charset=" . $this->encoding);
         $this->model = new Model();
-        $this->safebox = Safebox::getInstance();
-        $this->user = $this->safebox->get('user');
+        // $this->safebox = Safebox::getInstance();
+        // $this->user = $this->safebox->get('user');
         // if ($this->user == null) {
         //     $this->user = Common::autoLoginUserInfo();
         //     $this->safebox->set('user', $this->user);
         // }
-        $config = Config::getInstance();
-        $site_config = $config->get("globals");
-        $this->assign('seo_title', $site_config['site_name']);
-        $this->assign('site_title', $site_config['site_name']);
-        $cart = Cart::getCart();
-        $this->cart = $cart->all();
-        $this->selectcart = $this->cart;
-        $this->assign("cart", $this->cart);
+        // $config = Config::getInstance();
+        // $site_config = $config->get("globals");
+        // $this->assign('seo_title', $site_config['site_name']);
+        // $this->assign('site_title', $site_config['site_name']);
+        // $cart = Cart::getCart();
+        // $this->cart = $cart->all();
+        // $this->selectcart = $this->cart;
+        // $this->assign("cart", $this->cart);
 
     }
 
