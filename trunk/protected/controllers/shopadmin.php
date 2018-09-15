20076ee30d2f4b94c537d54420d3c82887519da6
qianfeifan <893645913@qq.com>
2018-09-15 17:07:43 +0800

    test

--- trunk/protected/controllers/shopadmin.php
+++ trunk/protected/controllers/shopadmin.php
@@ -108,7 +108,10 @@ class ShopadminController extends Controller {
             $v['imglist'] = isset($imglist[$v['id']]) ? $imglist[$v['id']] : array();
 
             $v['express_status'] = $writelist[$v['id']] == count($v['imglist']) ? 'finished' : 'inprogress';
-            // $v['img'] = isset($v['imglist'][0]['img']) ? $v['imglist'][0]['img'] : '';
+            $v['img'] = isset($v['imglist'][0]['img']) ? $v['imglist'][0]['img'] : '';
+        }
+        if($orders[0]['id']==2368) {
+            var_dump($orders);die;
         }
         unset($v);
         //处理过期订单状态
