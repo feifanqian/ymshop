e7316a9f02619f35bb41e122adbbe193d36309b0
qianfeifan <893645913@qq.com>
2018-09-15 16:40:34 +0800

    test

--- trunk/protected/controllers/districtadmin.php
+++ trunk/protected/controllers/districtadmin.php
@@ -2272,8 +2272,8 @@ class DistrictadminController extends Controller
         } else {
             $is_shop = 0;
         }
-        $customer = $this->model->table('customer as c')->fields('c.real_name,c.mobile,u.nickname')->join('left join user as u on c.user_id=u.id')->where('c.user_id='.$user_id)->find();
-        $promoter = $this->model->table('district_promoter')->fields('create_time')->where('user_id='.$user_id)->find();
+        $customer = $model->table('customer as c')->fields('c.real_name,c.mobile,u.nickname')->join('left join user as u on c.user_id=u.id')->where('c.user_id='.$user_id)->find();
+        $promoter = $model->table('district_promoter')->fields('create_time')->where('user_id='.$user_id)->find();
         $result['real_name'] = $customer['real_name'];
         $result['nickname'] = $customer['nickname'];
         $result['mobile'] = $customer['mobile'];
