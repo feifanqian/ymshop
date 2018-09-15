c27b330a644fe3381fe9df7014a46f4e9410ed12
qianfeifan <893645913@qq.com>
2018-09-15 09:48:06 +0800

    test

--- trunk/protected/controllers/content.php
+++ trunk/protected/controllers/content.php
@@ -451,9 +451,14 @@ class ContentController extends Controller {
             $content = array();
             if ($type == 2) {
                 $id = Req::args("id");
-                var_dump($id);die;
-                foreach ($path as $key => $value) {
-                    $content[$key] = array('path' => $value, 'url' => $url[$key], 'title' => $title[$key],'width' => $width[$key],'height' => $height[$key],'x_coor' => $x_coor[$key],'y_coor' => $y_coor[$key],'update_time' => $update_time[$key],'end_time' => $end_time[$key]);
+                if($id==85) {
+                    foreach ($path as $key => $value) {
+                        $content[$key] = array('path' => $value, 'url' => $url[$key], 'title' => $title[$key],'width' => $width[$key],'height' => $height[$key],'x_coor' => $x_coor[$key],'y_coor' => $y_coor[$key],'update_time' => $update_time[$key],'end_time' => $end_time[$key]);
+                    }
+                } else {
+                    foreach ($path as $key => $value) {
+                        $content[$key] = array('path' => $value, 'url' => $url[$key], 'title' => $title[$key]);
+                    }
                 }
             } else {
                 $content[0] = array('path' => $path[0], 'url' => $url[0], 'title' => $title[0]);
