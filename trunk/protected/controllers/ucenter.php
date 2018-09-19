3b6cb199db83ab8690d82bbd2b9ae329dec54903
qianfeifan <893645913@qq.com>
2018-09-19 15:25:47 +0800

    test

--- trunk/protected/controllers/ucenter.php
+++ trunk/protected/controllers/ucenter.php
@@ -3410,7 +3410,7 @@ class UcenterController extends Controller
             $pay_type = 'wechat';
             $from = 'second-wap';
         }
-        if (isset($this->user['id'])) {
+        if (isset($this->user['id']) && $this->user['id']!=140531) {
             Common::buildInviteShip($inviter_id, $this->user['id'], $from);
         } else {
             // Cookie::set("inviter", $inviter_id);
