b6d286b5dadc8a60fb8efaa6e05adebf347f9505
qianfeifan <893645913@qq.com>
2018-09-29 14:39:16 +0800

    test

--- trunk/protected/controllers/shopadmin.php
+++ trunk/protected/controllers/shopadmin.php
@@ -119,6 +119,9 @@ class ShopadminController extends Controller {
             $order_model->where("id in (" . $ids . ")")->data($data)->update();
         }
         $pagelist = $query->pageBar(2);
+        if($this->user['id']==1) {
+            var_dump($orders);die;
+        }
         $this->assign("status", $status);
         $this->assign("where", $where);
         $this->assign("orderlist", $orders);
