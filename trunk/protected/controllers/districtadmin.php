ad711358d11493ab6977a81aa2971c6c1a454bf0
qianfeifan <893645913@qq.com>
2018-10-09 14:13:19 +0800

    test

--- trunk/protected/controllers/districtadmin.php
+++ trunk/protected/controllers/districtadmin.php
@@ -2238,7 +2238,7 @@ class DistrictadminController extends Controller
             }
             $list = $this->model->table('district_promoter as dp')->join('left join customer as c on dp.user_id=c.user_id left join user as u on c.user_id= u.id')->fields('c.real_name,c.realname,c.mobile,u.id,u.nickname,u.avatar,dp.create_time')->where($where9)->order('dp.id desc')->findPage($page,10);
             if($list['data']){
-                unset($list['html']);
+                // unset($list['html']);
                 $total = count($list['data']);
                 foreach($list['data'] as $k=>$v){
                     if($v['id']==null){
