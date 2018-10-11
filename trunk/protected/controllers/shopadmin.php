dc9ac7ad2ca146086d9460c376c33d2ba0167577
qianfeifan <893645913@qq.com>
2018-10-11 18:57:17 +0800

    test

--- trunk/protected/controllers/shopadmin.php
+++ trunk/protected/controllers/shopadmin.php
@@ -111,7 +111,7 @@ class ShopadminController extends Controller {
             }
         }
         $orders_list = $this->model->table("order_goods AS og")->fields("o.id,go.img,o.accept_name,o.pay_time,o.delivery_status,og.goods_nums")->join("left join goods AS go ON og.goods_id=go.id left join order as o on og.order_id=o.id")->where("FIND_IN_SET('{$this->user['id']}',o.shop_ids) and o.status BETWEEN 1 AND 4 and o.pay_status=1 and o.is_del=0 and o.is_robot=0 and og.shop_id = '{$this->user['id']}'")->order('o.id desc')->findAll($page,10);
-        var_dump($orders_list);die;
+        // var_dump($orders_list);die;
         foreach ($orders_list as $k => $v) {
             $orders_list[$k]['img'] = "https://ymlypt.b0.upaiyun.com".$v['img'];  
         }
