214a4389ecff31352ac10f3090e1c7db3f486aa5
qianfeifan <893645913@qq.com>
2018-10-11 09:49:08 +0800

    test

--- trunk/protected/controllers/districtadmin.php
+++ trunk/protected/controllers/districtadmin.php
@@ -2221,6 +2221,7 @@ class DistrictadminController extends Controller
            echo json_encode(array("status" => 'success', 'msg' => '成功'));
             exit();
         } else {
+            var_dump($res['ysepay_merchant_register_accept_response']['sub_msg']);die;
             echo json_encode(array("status" => 'error', 'msg' => $res['ysepay_merchant_register_accept_response']['sub_msg']));
             exit();
         }    
