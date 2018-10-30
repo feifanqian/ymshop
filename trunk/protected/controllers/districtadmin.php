fd6d6c5ecd34594a8792da75840d088a884df483
qianfeifan <893645913@qq.com>
2018-10-30 14:11:42 +0800

    test

--- trunk/protected/controllers/districtadmin.php
+++ trunk/protected/controllers/districtadmin.php
@@ -1610,8 +1610,8 @@ class DistrictadminController extends Controller
               
             $params['version'] = '3.0';
             $biz_content_arr = array(
-                // 'merchant_no'=>'yuanmeng'.$shop_check['user_id'].rand(11,99),
-                'merchant_no'=>'yuanmeng',
+                'merchant_no'=>'yuanmeng'.$shop_check['user_id'].rand(11,99),
+                // 'merchant_no'=>'yuanmeng',
                 'cust_type'=>$cust_type,
                 'token'=>$shop_check['token'],
                 'another_name'=>$name,
