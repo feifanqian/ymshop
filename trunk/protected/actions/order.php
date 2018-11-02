ce8429d33a0365e0afbcac26b74d5df4b347c5f3
qianfeifan <893645913@qq.com>
2018-11-02 11:41:37 +0800

    test

--- trunk/protected/actions/order.php
+++ trunk/protected/actions/order.php
@@ -424,8 +424,8 @@ class OrderAction extends Controller {
                         $data['prom_id'] = $id;
                         $order_type = 7;
                 }else if($type=="pointflash"){
-                        var_dump($product_id);die;
                         $product_id = Filter::int($product_id[0]);
+                        var_dump($product_id);die;
                         $num = isset($buy_num[0])?Filter::int($buy_num[0]):1;
                         if ($num < 1)
                             $num = 1;
