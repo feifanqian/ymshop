3a63c4a1c57eac780326d9787aa07cdc226676be
qianfeifan <893645913@qq.com>
2018-11-02 11:15:07 +0800

    test

--- trunk/protected/actions/order.php
+++ trunk/protected/actions/order.php
@@ -435,7 +435,8 @@ class OrderAction extends Controller {
                                 ->find();
                         $pointflash = $model->table("pointflash_sale")->where("id=$id")->find();
                         if(empty($pointflash)||empty($item)){
-                            var_dump($id);die;
+                            var_dump($item);
+                            var_dump($pointflash);die;
                             $this->code = 1214;
                             return;
                         }
